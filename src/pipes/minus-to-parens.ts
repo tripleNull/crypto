01aff822ef4dfc2911a89c820cd19d615f2869ff
Triple Null <tripleNull@protonmail.com>
2018-03-31 19:55:11 -0400

    remove minus sign to parens

--- src/pipes/minus-to-parens.ts
+++ src/pipes/minus-to-parens.ts
@@ -2,15 +2,15 @@ import { CryptoService } from './../services/crypto.service';
 import { Pipe, PipeTransform } from '@angular/core';
 
 
-@Pipe({
-  name: 'minusSignToParens'
-})
-export class MinusSignToParens implements PipeTransform {
+//@Pipe({
+  //name: 'minusSignToParens'
+//})
+//export class MinusSignToParens implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return value.charAt(0) === '-' ?
-           '(' + value.substring(1, value.length) + ')' :
-           value;
-  }
+  //transform(value: any, args?: any): any {
+  //  return value.charAt(0) === '-' ?
+  //         '(' + value.substring(1, value.length) + ')' :
+  //         value;
+  //}
 
-}
+//}
